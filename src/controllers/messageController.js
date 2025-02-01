5a9183eff920a66c2e68ff0a08ec159e9834dd16
uiux@okdollar frontend <uiux@okdollar.com>
2025-02-01 16:12:23 +0630

    last message

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -42,3 +42,29 @@ export const getMessage = async(req,res)=>{
         return res.status(500).json(error)
     }
 }
+
+export const getLastMessage = async(req,res)=>{
+    const {chatId} = req.params;
+    try {
+        const findChat = await Chats.find({_id:chatId})
+        if(findChat.length === 0){
+            return res.status(404).json({message:'conversation does not exist.'})
+        }
+        const findMessage = await Message.find({chatId:chatId})
+        if(findMessage.length === 0){
+            return res.status(200).json({message:'Message not found'})
+        }
+        const lastMessage = findMessage[findMessage.length - 1]
+
+        const getData= {
+            success:true,
+            status:200,
+            data:{
+                message: lastMessage.meassage
+            }
+        }
+        return res.status(200).json(getData)
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+}
--- src/routes/messageRoute.js
+++ src/routes/messageRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import protectedRoute from '../middleware/protectedRoute.js';
-import { createMessage, getMessage } from '../controllers/messageController.js';
+import { createMessage, getLastMessage, getMessage } from '../controllers/messageController.js';
 
 const router = express.Router()
 
 router.post('/',protectedRoute,createMessage)
 router.get("/:chatId",protectedRoute,getMessage)
+router.get("/last/:chatId",protectedRoute,getLastMessage)
 
 export default router;
